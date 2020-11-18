f30cee8cb9cf5815ed4d3a75b432253cfa8156ce
oushiun <xzy.xiongzhenyu@gmail.com>
2020-11-18 12:20:39 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <signature msg="Welcome to Your Vue.js App" />
+    </div>
+</template>
+
+<script>
+import signature from './components/signature.vue'
+
+export default {
+    name: 'app',
+    components: {
+        signature
+    }
+}
+</script>
+
+<style>
+body {
+    margin: 0;
+    padding: 0;
+}
+#app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /* text-align: center; */
+    color: #2c3e50;
+    /* margin-top: 60px; */
+}
+</style>
---
+++ src/components/signature.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="signature">
+        <div class="pigaitimuxiangq_continer" id="question"><span class="tihao">1.</span><span class="tigan" style="white-space:pre-wrap;">完形填空
+                Many parents want their children to be famous one day. But do children have the same </span><span id="3406899_1" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_1" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;?A new 2 </span><span id="3406899_3" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_3" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;, Hi, Ke'ai, is on at Beijing Children's Art Theater(剧院). It tells the story of a boy called Ke'ai. His parents would like him to become a painter or a </span><span id="3406899_5" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_5" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;one day. So they teach him to </span><span id="3406899_7" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_7" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;and to play the violin, but Ke'ai doesn't enjoy these activities. Then one day Ke'ai's parents see Mo Yan win an award(奖) for his works, and they want him to be a writer, too.
+                " </span><span id="3406899_9" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_9" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;do they want me to be someone else?" Ke'ai asks and says, "I </span><span id="3406899_11" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_11" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;want to be myself."
+                The play(话剧) shows us that it is </span><span id="3406899_13" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_13" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;for parents to learn to understand their children. It helps parents to </span><span id="3406899_15" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_15" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;what kids really want to be in the future.
+                Young audiences(观众) enjoy the story, and also the </span><span id="3406899_17" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_17" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;in the play. There are two songs in the play. One of them, "Ke'ai's Song", is very easy to learn, so the audiences can sing the song on their way home </span><span id="3406899_19" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_19" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;the play!
+                1. A. job B. dreams C. stories D. hobbies
+                2. A. song B. film C. play D. cartoon
+                3. A. writer B. teacher C. sportsman D. musician
+                4. A. paint B. write C. run D. drive
+                5. A. How B. Why C. When D. Where
+                6. A. only B. seldom C. never D. sometimes
+                7. A. easy B. important C. interesting D. exciting
+                8. A. talk about B. worry about C. think about D, talk of
+                9. A. light B. clothes C. skill D. music
+                10. A. after B. before C. in D. at</span>
+
+            <button onclick="alert(123)">这是一个测试穿透按钮</button>
+            <span class="tigan" style="white-space:pre-wrap;">完形填空
+                Many parents want their children to be famous one day. But do children have the same </span><span id="3406899_1" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_1" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;?A new 2 </span><span id="3406899_3" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_3" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;, Hi, Ke'ai, is on at Beijing Children's Art Theater(剧院). It tells the story of a boy called Ke'ai. His parents would like him to become a painter or a </span><span id="3406899_5" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_5" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;one day. So they teach him to </span><span id="3406899_7" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_7" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;and to play the violin, but Ke'ai doesn't enjoy these activities. Then one day Ke'ai's parents see Mo Yan win an award(奖) for his works, and they want him to be a writer, too.
+                " </span><span id="3406899_9" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_9" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;do they want me to be someone else?" Ke'ai asks and says, "I </span><span id="3406899_11" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_11" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;want to be myself."
+                The play(话剧) shows us that it is </span><span id="3406899_13" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_13" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;for parents to learn to understand their children. It helps parents to </span><span id="3406899_15" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_15" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;what kids really want to be in the future.
+                Young audiences(观众) enjoy the story, and also the </span><span id="3406899_17" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_17" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;in the play. There are two songs in the play. One of them, "Ke'ai's Song", is very easy to learn, so the audiences can sing the song on their way home </span><span id="3406899_19" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_19" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;the play!
+                1. A. job B. dreams C. stories D. hobbies
+                2. A. song B. film C. play D. cartoon
+                3. A. writer B. teacher C. sportsman D. musician
+                4. A. paint B. write C. run D. drive
+                5. A. How B. Why C. When D. Where
+                6. A. only B. seldom C. never D. sometimes
+                7. A. easy B. important C. interesting D. exciting
+                8. A. talk about B. worry about C. think about D, talk of
+                9. A. light B. clothes C. skill D. music
+                10. A. after B. before C. in D. at</span>
+            <span class="tigan" style="white-space:pre-wrap;">完形填空
+                Many parents want their children to be famous one day. But do children have the same </span><span id="3406899_1" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_1" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;?A new 2 </span><span id="3406899_3" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_3" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;, Hi, Ke'ai, is on at Beijing Children's Art Theater(剧院). It tells the story of a boy called Ke'ai. His parents would like him to become a painter or a </span><span id="3406899_5" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_5" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;one day. So they teach him to </span><span id="3406899_7" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_7" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;and to play the violin, but Ke'ai doesn't enjoy these activities. Then one day Ke'ai's parents see Mo Yan win an award(奖) for his works, and they want him to be a writer, too.
+                " </span><span id="3406899_9" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_9" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;do they want me to be someone else?" Ke'ai asks and says, "I </span><span id="3406899_11" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_11" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;want to be myself."
+                The play(话剧) shows us that it is </span><span id="3406899_13" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_13" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;for parents to learn to understand their children. It helps parents to </span><span id="3406899_15" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_15" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;what kids really want to be in the future.
+                Young audiences(观众) enjoy the story, and also the </span><span id="3406899_17" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_17" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;in the play. There are two songs in the play. One of them, "Ke'ai's Song", is very easy to learn, so the audiences can sing the song on their way home </span><span id="3406899_19" class="underlineSpan short"><span class="underlineSpanInner daanSpan daan" id="timu_bijiController_0_0_19" style="position: relative;"></span></span><span class="tigan" style="white-space:pre-wrap;">&nbsp;the play!
+                1. A. job B. dreams C. stories D. hobbies
+                2. A. song B. film C. play D. cartoon
+                3. A. writer B. teacher C. sportsman D. musician
+                4. A. paint B. write C. run D. drive
+                5. A. How B. Why C. When D. Where
+                6. A. only B. seldom C. never D. sometimes
+                7. A. easy B. important C. interesting D. exciting
+                8. A. talk about B. worry about C. think about D, talk of
+                9. A. light B. clothes C. skill D. music
+                10. A. after B. before C. in D. at</span>
+        </div>
+        <!-- pointer-events:none -->
+        <canvas id='mycanvas' class="mycanvas" ref="mycanvas" :width="500" :height="400" style="position: absolute; top: 0px; left:0px; z-index: 1;touch-action:pan-y;"></canvas>
+
+        <button @click="undo">笔迹撤销</button>
+        <button @click="clear">笔迹清空</button>
+    </div>
+</template>
+
+<script>
+import SignaturePad from '@/assets/js/signature_pad'
+
+export default {
+    data () {
+        return {
+            signaturePad: null,
+            config: {
+                penColor: "red",   //笔刷颜色
+                minWidth: 1,       //最小宽度
+                maxWidth: 2,       //最大宽度
+            }
+        }
+    },
+    mounted () {
+        let _this = this
+        let canvas = document.querySelector('.mycanvas')
+        this.signaturePad = new SignaturePad(canvas, this.config)
+        canvas.height = document.getElementById('question').clientHeight
+        canvas.width = document.getElementById('question').clientWidth
+
+        window.onresize = function () { // 定义窗口大小变更通知事件
+            var data = _this.signaturePad.toData();
+            let canvas = document.querySelector('.mycanvas')
+            canvas.height = document.getElementById('question').clientHeight
+            canvas.width = document.getElementById('question').clientWidth
+            _this.signaturePad.fromData(data);
+        }
+    },
+    methods: {
+        undo () {
+            var data = this.signaturePad.toData();
+            if (data) {
+                data.pop(); // remove the last dot or line
+                this.signaturePad.fromData(data);
+            }
+        },
+        clear () {
+            this.signaturePad.clear()
+        }
+    }
+}
+</script>
+
+<style scoped>
+@font-face {
+    font-family: "Microsoft-YaHei-Mono";
+    src: url("https://quangou-001.oss-cn-hangzhou.aliyuncs.com/nanchang-resource/www/resource/media/Microsoft-YaHei-Mono.woff")
+        format("woff");
+}
+
+.signature {
+    font-family: "Microsoft-YaHei-Mono";
+}
+</style>
